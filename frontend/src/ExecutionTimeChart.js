e73002a40e39f9340531ccddd19c9077b4aacda1
Akash Bilgi <akash.bilgi@gmail.com>
2023-08-25 00:43:01 -0700

    cleaning up

--- frontend/src/ExecutionTimeChart.js
+++ frontend/src/ExecutionTimeChart.js
@@ -4,14 +4,14 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'rec
 const ExecutionTimeChart = ({ data }) => {
   return (
     <div className="execution-time-chart-container">
-      <h2>Execution Time Comparison Chart</h2>
-      <BarChart width={300} height={300} data={data}>
+      <h2>Execution Time Chart</h2>
+      <BarChart width={250} height={300} data={data}>
         <CartesianGrid strokeDasharray="7" />
         <XAxis dataKey="name" tickFormatter={() => ''} />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="value" fill="#8884d8" name="Execution Time" />
+        <Bar dataKey="value" fill="#8884d8" name="Time(seconds)" />
       </BarChart>
     </div>
   );
